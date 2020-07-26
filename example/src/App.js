e00492a24858cec75f18a33e4921811263f4de65
aditya <kumar.93@iitj.ac.in>
2020-07-26 11:38:02 +0530

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'app-top-bar-cadmea'
+import 'app-top-bar-cadmea/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
